cc4de39e8ff4e28d39df3a22262e9325737bae4a
cmon bruh <cotiajviemmladyxD@eksdi.com>
2023-04-25 13:36:57 +0200

    opengl

---
+++ include/config.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+#define DEBUG_PRINT 1
+
+void print(std::string msg);
+
+using namespace std;
